4231e34fc566fad90d1f0429832a577b00856150
atakhatri <atakhatri905@gmail.com>
2025-12-10 15:22:13 +0530

    clean project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-br from-blue-950 to-blue-800">
+      <h1 className="text-4xl font-bold text-white">
+        Welcome to your clean project!
+      </h1>
+    </main>
+  );
+}
